6c5d96befbd32f7a912c97db895b053f2ba7210b
mustifanci <mustifanci@gmail.com>
2022-05-08 23:59:25 +0300

    5.olanı yarım kaldı

--- 05-menu/setup/src/Menu.js
+++ 05-menu/setup/src/Menu.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
-const Menu = () => {
-  return <h2>menu component</h2>;
+const Menu = ({items}) => {
+  return ( 
+  <div className='section-center'>
+    {items.map((menuItem)=> {
+      const {id,title,price,img} = menuItem;
+      return (
+        <article key={id} className='menu-item'>
+          <img src={img} alt={title} className='photo'></img>
+        </article>
+      );
+    })}
+  </div>
+  );
 };
 
 export default Menu;
